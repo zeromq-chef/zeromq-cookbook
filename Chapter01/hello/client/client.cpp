e4f37dcc16b440e17f0556665bdfdf5c22ae620f
paranlee <p4ranlee@gmail.com>
2021-12-06 13:51:16 +0900

    책 기준 Hello world Server Client 예제 추가

--- Chapter01/hello/client/client.cpp
+++ Chapter01/hello/client/client.cpp
@@ -1,38 +1,46 @@
 // Example 1-4. Hello World client (hwclient.c)
 //
-//  Hello World client
-//  Connects REQ socket to tcp://localhost:5555
-//  Sends "Hello" to server, expects "World" back
+// Hello World client
+// Connects REQ socket to tcp://localhost:5555
+// Sends "Hello" to server, expects "World" back
 //
 #include <zmq.h>
 #include <string.h>
 #include <stdio.h>
 
-int main (void)
-{
-    void *context = zmq_init (1);
+#include<windows.h>
 
-    //  Socket to talk to server
-    printf ("Connecting to hello world server\n");
-    void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://localhost:5555");
+int main(void)
+{
+    void *context = zmq_ctx_new();
+
+    // Socket to talk to server
+    printf("Connecting to hello world server...\n");
+    void *requester = zmq_socket(context, ZMQ_REQ);
+
+    zmq_connect(requester, "tcp://localhost:5555");
 
     int request_nbr;
-    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
+    for (request_nbr = 0; request_nbr != 10; request_nbr++)
+    {
         zmq_msg_t request;
         zmq_msg_init_size (&request, 5);
         memcpy (zmq_msg_data (&request), "Hello", 5);
-        printf ("Sending Hello %d\n", request_nbr);
-        zmq_sendmsg (requester, &request, 0);
+        printf ("Sending Hello %d...\n", request_nbr);
+        zmq_msg_send (&request, requester, 0);
         zmq_msg_close (&request);
-
+        
         zmq_msg_t reply;
         zmq_msg_init (&reply);
-        zmq_recvmsg (requester, &reply, 0);
+        zmq_msg_recv (&reply, requester, 0);
         printf ("Received World %d\n", request_nbr);
         zmq_msg_close (&reply);
     }
-    zmq_close (requester);
-    zmq_term (context);
+
+    Sleep(2);
+
+    zmq_close(requester);
+    zmq_ctx_destroy(context);
+
     return 0;
 }
--- Chapter01/hello/server/server.cpp
+++ Chapter01/hello/server/server.cpp
@@ -1,42 +1,49 @@
 // Example 1-1. Hello World server (hwserver.c)
 //
-//  Hello World server
-//  Binds REP socket to tcp://*:5555
-//  Expects "Hello" from client, replies with "World"
+// Hello World server
+// Binds REP socket to tcp://*:5555
+// Expects "Hello" from client, replies with "World"
 //
 #include <zmq.h>
 #include <stdio.h>
 #include <string.h>
+
+// window 전용
 #include <windows.h>
 
-int main (void)
+
+int main(void)
 {
-    void *context = zmq_init (1);
+    void *context = zmq_ctx_new();
 
-    //  Socket to talk to clients
-    void *responder = zmq_socket (context, ZMQ_REP);
-    zmq_bind (responder, "tcp://*:5555");
+    // Socket to talk to clients
+    void *responder = zmq_socket(context, ZMQ_REP);
+    
+    zmq_bind(responder, "tcp://*:5555");
 
-    while (1) {
-        //  Wait for next request from client
+    while (1)
+    {
+        // Wait for next request from client
         zmq_msg_t request;
-        zmq_msg_init (&request);
-        zmq_recvmsg (responder, &request, 0);
-        printf ("Received Hello\n");
-        zmq_msg_close (&request);
+        zmq_msg_init(&request);
+        zmq_msg_recv(&request, responder, 0);
+        printf("Received Hello\n");
+        zmq_msg_close(&request);
 
-        //  Do some 'work'
-        Sleep (1);
+        // Do some 'work'
+        Sleep(1);
 
-        //  Send reply back to client
+        // Send reply back to client
         zmq_msg_t reply;
-        zmq_msg_init_size (&reply, 5);
-        memcpy (zmq_msg_data (&reply), "World", 5);
-        zmq_sendmsg (responder, &reply, 0);
-        zmq_msg_close (&reply);
+        zmq_msg_init_size(&reply, 5);
+        memcpy(zmq_msg_data(&reply), "World", 5);
+        zmq_msg_send(&reply, responder, 0);
+        zmq_msg_close(&reply);
     }
-    //  We never get here but if we did, this would be how we end
-    zmq_close (responder);
-    zmq_term (context);
+
+    // We never get here but if we did, this would be how we end
+    zmq_close(responder);
+    zmq_ctx_destroy(context);
+
     return 0;
 }
